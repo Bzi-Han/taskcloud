c4e28ae9d8dd035af7b2538902d5f795b7989cb2
Bzi_Han <rosebzi@foxmail.com>
2023-01-13 18:58:13 +0800

    Add docker-compose files

---
+++ datas/init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ ! -f "/enviroment.lock" ]; then
+    cp /etc/apt/sources.list /etc/apt/sources.list.bak
+
+    sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list
+
+    sed -i 's/security.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
+
+    apt-get update
+
+    apt-get install python3.10-dev -y
+
+    touch /enviroment.lock
+fi
